f435e47464ada30f6cff944d8e3a69fdc966a52a
Job Candidate <anonymous@loco2.com>
2022-05-21 23:55:04 +0200

    impelement connection#train_name

--- lib/rail_search/connection.rb
+++ lib/rail_search/connection.rb
@@ -2,7 +2,7 @@
 
 module RailSearch
   class Connection
-    attr :start, :finish, :departure_time, :arrival_time
+    attr :start, :finish, :departure_time, :arrival_time, :train_name
 
     def initialize(raw_connection)
       connection_elements = raw_connection.elements
@@ -11,12 +11,13 @@ module RailSearch
       @finish         = connection_elements['Finish'].text
       @departure_time = connection_elements['DepartureTime'].text
       @arrival_time   = connection_elements['ArrivalTime'].text
+      @train_name   = connection_elements['TrainName'].text
       @fares          = Fares.new(connection_elements['Fares'])
-      # TODO: @duration, @train_name
+      # TODO: @duration
     end
 
     def inspect
-      { start: @start, finish: @finish, departure_time: @departure_time, arrival_time: @arrival_time, fares: @fares }
+      { start: @start, finish: @finish, departure_time: @departure_time, arrival_time: @arrival_time, train_name: @train_name, fares: @fares }
     end
   end
 end
