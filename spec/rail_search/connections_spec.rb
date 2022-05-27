bfa72bbe4d7884ece0a28da9ee742cc7a4cfb47d
Job Candidate <anonymous@loco2.com>
2022-05-28 01:59:53 +0200

    implement connections specs

---
+++ spec/rail_search/connections_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rexml'
+
+RSpec.describe RailSearch::Connections do
+  subject { described_class.new(raw_connections) }
+
+  response = File.read('spec/fixtures/search.xml').to_s
+
+  xml = REXML::Document.new(response)
+
+  let(:raw_connections) { xml.get_elements('//SearchResult')[0].get_elements('//Connections')[0] }
+
+  it { expect(subject.count).to eq(raw_connections.elements.count) }
+
+  it { is_expected.to all(be_an_instance_of(RailSearch::Connection)) }
+
+  describe '#train_changes_count' do
+    it { expect(subject.train_changes_count).to eq(raw_connections.elements.count - 1) }
+  end
+end
