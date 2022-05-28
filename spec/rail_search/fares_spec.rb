a2a7b10128720d22aa0eb97796bc01a9577351c6
Job Candidate <anonymous@loco2.com>
2022-05-28 02:00:11 +0200

    implement fares specs

---
+++ spec/rail_search/fares_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rexml'
+
+RSpec.describe RailSearch::Fares do
+  subject { described_class.new(raw_fares) }
+
+  response = File.read('spec/fixtures/search.xml').to_s
+
+  xml = REXML::Document.new(response)
+
+  let(:raw_fares) { xml.get_elements('//SearchResult')[0].get_elements('//Connections')[0].get_elements('//Fares')[0] }
+
+  it { expect(subject.count).to eq(raw_fares.elements.count) }
+
+  it { is_expected.to all(be_an_instance_of(RailSearch::Fare)) }
+end
